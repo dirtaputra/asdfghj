00f86db87c908a2a20c397d7ff11bcbc6af00963
Dirta Putra Anggara <dirtaputraanggara@gmail.com>
2019-03-25 01:55:16 +0700

    bug fix

---
+++ profile.php
@@ -0,0 +1,308 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!-- start: Meta -->
+	<meta charset="utf-8">
+	<title>Toko giacinta arianne belen ananta</title> 
+	<!-- end: Meta -->
+	
+	<!-- start: Mobile Specific -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<!-- end: Mobile Specific -->
+	
+	<!-- start: Facebook Open Graph -->
+	<meta property="og:title" content=""/>
+	<meta property="og:description" content=""/>
+	<meta property="og:type" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:image" content=""/>
+	<!-- end: Facebook Open Graph -->
+
+    <!-- start: CSS --> 
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/bootstrap-responsive.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Droid+Sans:400,700">
+	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Droid+Serif">
+	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Boogaloo">
+	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Economica:700,400italic">
+    <style type="text/css">
+    .konten
+{
+    margin-top: 20px;
+    margin-left: 350px;
+    margin-right: 15px;
+	color: black;
+	font-family: calibri;
+    font-size: 20px;
+    font-weight: none;	
+}
+.komentar
+{
+    position: center;
+    margin-top: 20px;
+    margin-left: 340px;
+    margin-right: 15px;
+	color: gray;
+	font-family: calibri;
+    font-size: 14px;
+    font-weight: none;	
+    border: 1px solid #;
+    width: 700px;
+    height: 260px;
+    overflow: scroll;
+}
+    </style>
+	<!-- end: CSS -->
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+</head>
+<body>
+    
+	<!--start: Header -->
+	<header>
+		
+		<!--start: Container -->
+		<div class="container">
+			
+			<!--start: Row -->
+			<div class="row">
+					
+				<!--start: Logo -->
+				
+				<!--end: Logo -->
+					
+				<!--start: Navigation -->
+				<div class="span9">
+					
+					<div class="navbar navbar-inverse">
+			    		<div class="navbar-inner">
+			          		<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+			            		<span class="icon-bar"></span>
+			            		<span class="icon-bar"></span>
+			            		<span class="icon-bar"></span>
+			          		</a>
+			          		<div class="nav-collapse collapse">
+			            		<ul class="nav">
+			              			<li><a href="index.php">Home</a></li>
+			              			<li><a href="produk.php">Produk Kami</a></li>
+									<li  class="active"><a href="profile.php">Profile</a></li>
+                                    <li><a href="detail.php">Keranjang</a></li>
+                                    <li><a href="logout.php">Logout</a></li>
+			            		</ul>
+			          		</div>
+			        	</div>
+			      	</div>
+					
+				</div>	
+				<!--end: Navigation -->
+					
+			</div>
+			<!--end: Row -->
+			
+		</div>
+		<!--end: Container-->			
+			
+	</header>
+	<!--end: Header-->
+	
+	<!-- start: Page Title -->
+	<div id="page-title">
+
+		<div id="page-title-inner">
+
+			<!-- start: Container -->
+			<div class="container">
+
+				<h2><i class="ico-stats ico-white"></i>Profile</h2>
+
+			</div>
+			<!-- end: Container  -->
+
+		</div>	
+
+	</div>
+	<!-- end: Page Title -->
+	
+	<!--start: Wrapper-->
+	<div id="wrapper">
+				
+		<!--start: Container -->
+    	<div class="container"> 
+        <center><div class="title" style="margin-left: ;"><h3>Profile...</h3></div></center>
+            <form id="formku" method="post" action="proses.php" onsubmit="return formCheck(this);">
+<table style="font-style: oblique; font-weight: bold; margin-left: 140px;">
+<tr>
+<td width="150">Nama</td>
+<td width="30">:</td>
+<td width="550"><span>giacinta arianne belen ananta</span></td>
+</tr>
+<tr>
+<td width="150">NO telp</td>
+<td width="30">:</td>
+<td width="550"><span>081235664089</span></td>
+</tr>
+<tr>
+<td width="150">Instagram</td>
+<td width="30">:</td>
+<td width="550">@giacinta.arianne</td>
+</tr>
+<tr>
+<td width="150">Sekolah</td>
+<td width="30">:</td>
+<td width="550">SMAK Kolese Santo Yusup Malang</td>
+</tr>
+<tr>
+<td width="150">Kelas</td>
+<td width="30">:</td>
+<td width="550">11 MIA 5</td>
+</tr>
+<tr>
+<td width="150">No Absen</td>
+<td width="30">:</td>
+<td width="550">17</td>
+</tr>
+</table>
+</form>
+</div>
+
+</div>
+		</div>
+		<!--end: Container-->
+				
+		<!--start: Container -->
+    	<div class="container">	
+      		
+			<hr>
+		
+			<!-- start Clients List -->	
+			
+			<!-- end Clients List -->
+		
+			<hr>
+		
+		</div>
+		<!--end: Container-->	
+
+	</div>
+	<!-- end: Wrapper  -->			
+
+    <!-- start: Footer Menu -->
+	<div id="footer-menu" class="hidden-tablet hidden-phone">
+
+		<!-- start: Container -->
+		<div class="container">
+			
+			<!-- start: Row -->
+			<div class="row">
+
+				<!-- start: Footer Menu Logo -->
+				<div class="span2">
+					<div id="footer-menu-logo">
+						<a href="#"><img src="img/logo-footer-menu.png" alt="logo" /></a>
+					</div>
+				</div>
+				<!-- end: Footer Menu Logo -->
+
+				<!-- start: Footer Menu Links-->
+				<div class="span9">
+					
+					<div id="footer-menu-links">
+
+						<ul id="footer-nav">
+
+							<li><a href="#">Kemeja</a></li>
+
+							<li><a href="#">Kaos</a></li>
+
+							<li><a href="#">Sweater</a></li>
+
+							<li><a href="#">Jacket</a></li>
+							
+							<li><a href="#">Pants & Jeans</a></li>
+
+						</ul>
+
+					</div>
+					
+				</div>
+				<!-- end: Footer Menu Links-->
+
+				<!-- start: Footer Menu Back To Top -->
+				<div class="span1">
+						
+					<div id="footer-menu-back-to-top">
+						<a href="#"></a>
+					</div>
+				
+				</div>
+				<!-- end: Footer Menu Back To Top -->
+			
+			</div>
+			<!-- end: Row -->
+			
+		</div>
+		<!-- end: Container  -->	
+
+	</div>	
+	<!-- end: Footer Menu -->
+
+	<!-- start: Footer -->
+	<div id="footer">
+		
+		<!-- start: Container -->
+		<div class="container">
+			
+			<!-- start: Row -->
+			
+			<!-- end: Row -->	
+			
+		</div>
+		<!-- end: Container  -->
+
+	</div>
+	<!-- end: Footer -->
+
+	<!-- start: Copyright -->
+	
+	<!-- end: Copyright -->
+
+<!-- start: Java Script -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="js/jquery-1.8.2.js"></script>
+<script src="js/bootstrap.js"></script>
+<script src="js/flexslider.js"></script>
+<script src="js/carousel.js"></script>
+<script src="js/jquery.cslider.js"></script>
+<script src="js/slider.js"></script>
+<script def src="js/custom.js"></script>
+
+<script src="jquery.validate.js"></script>
+    <script>
+    $(document).ready(function(){
+        $("#formku").validate();
+    });
+    </script> 
+    
+    <style type="text/css">
+    label.error {
+        color: red;
+        padding-left: .5em;
+    }
+    </style>
+    
+    <script type="text/javascript">
+    x=0;
+    $(document).ready(function(){
+       $(".komentar").scroll(function(){
+        $("span").text(x+=1);
+       }); 
+    });
+    </script>
+<!-- end: Java Script -->
+
+</body>
+</html>	
---
+++ toko_online.sql
@@ -0,0 +1,218 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Mar 24, 2019 at 07:51 PM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.3.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `toko_online`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `fullname` varchar(30) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`user_id`, `username`, `password`, `fullname`) VALUES
+(3, 'admin', 'admin', 'hakko bio richard');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang`
+--
+
+CREATE TABLE `barang` (
+  `br_id` int(6) NOT NULL,
+  `br_nm` varchar(50) NOT NULL,
+  `br_item` int(4) NOT NULL,
+  `br_hrg` int(10) NOT NULL,
+  `br_stok` int(9) NOT NULL,
+  `br_satuan` varchar(20) NOT NULL,
+  `br_gbr` varchar(100) NOT NULL,
+  `ket` varchar(250) NOT NULL,
+  `br_sts` varchar(1) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `barang`
+--
+
+INSERT INTO `barang` (`br_id`, `br_nm`, `br_item`, `br_hrg`, `br_stok`, `br_satuan`, `br_gbr`, `ket`, `br_sts`) VALUES
+(1, 'Kaos Sudirman', 1, 60000, 12, 'Pcs', 'gambar/8.jpg', 'Bahan cvc bandung build up tanpa jahitan samping', 'Y'),
+(2, 'Kaos Skaters', 1, 65000, 24, 'Pcs', 'gambar/9.jpg', 'Bahan Cotton Combed 24s built up', 'Y'),
+(3, 'Kaos Emwe', 1, 80000, 30, 'Pcs', 'gambar/6.jpg', 'Bahan cvc bandung build up tanpa jahitan samping', 'Y'),
+(4, 'Kaos Begin', 1, 80000, 20, 'Pcs', 'gambar/7.jpg', 'Bahan cvc bandung build up tanpa jahitan samping', 'Y'),
+(5, 'Kaos Man City', 1, 60000, 30, 'Pcs', 'gambar/2.jpg', 'Bahan Cotton Combed 20s Jahitan samping', 'Y'),
+(6, 'Kaos H & L', 1, 75000, 20, 'Pcs', 'gambar/5.jpg', 'Bahan Cotton Combed 24s built up', 'Y'),
+(7, 'Kaos Cool Blue', 1, 70000, 20, 'Pcs', 'gambar/4.jpg', 'Bahan kardet jakarta', 'Y'),
+(8, 'Kaos Feed Me', 1, 65000, 12, 'Pcs', 'gambar/3.jpg', 'Bahan Cotton Combed 24s built up', 'Y'),
+(9, 'Kaos Art Hitam', 1, 55000, 20, 'Pcs', 'gambar/1.jpg', 'Bahan cvc bandung build up tanpa jahitan samping', 'Y');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `no_pem`
+--
+
+CREATE TABLE `no_pem` (
+  `nopem` varchar(4) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `no_pem`
+--
+
+INSERT INTO `no_pem` (`nopem`) VALUES
+('79');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pembelian`
+--
+
+CREATE TABLE `pembelian` (
+  `no_hdpem` varchar(10) NOT NULL,
+  `tgl_hdpem` date NOT NULL,
+  `usr_hdpem` int(11) NOT NULL,
+  `crkir_hdpem` tinyint(4) NOT NULL,
+  `crpem_hdpem` tinyint(4) NOT NULL,
+  `penerima_hdpem` varchar(50) NOT NULL,
+  `almt_pem` text NOT NULL,
+  `kp_pem` varchar(6) NOT NULL,
+  `kota_pem` varchar(30) NOT NULL,
+  `tlp` varchar(16) NOT NULL,
+  `rek_pem` varchar(50) NOT NULL,
+  `nmrek_pem` varchar(20) NOT NULL,
+  `bank_pem` varchar(25) NOT NULL,
+  `ct_pem` text NOT NULL,
+  `tot_hdpem` int(11) NOT NULL,
+  `sts_pem` varchar(1) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `hd_trpem` varchar(10) NOT NULL,
+  `br_trpem` varchar(6) NOT NULL,
+  `qty_trpem` int(11) NOT NULL,
+  `hrg_trpem` int(11) NOT NULL,
+  `sts_trpem` varchar(1) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id_usr` int(11) NOT NULL,
+  `nm_usr` varchar(50) NOT NULL,
+  `log_usr` varchar(20) NOT NULL,
+  `pas_usr` varchar(100) NOT NULL,
+  `email_usr` varchar(50) NOT NULL,
+  `almt_usr` text NOT NULL,
+  `kp_usr` varchar(6) NOT NULL,
+  `kota_usr` varchar(25) NOT NULL,
+  `tlp` varchar(20) NOT NULL,
+  `rek` varchar(30) NOT NULL,
+  `nmrek` varchar(30) NOT NULL,
+  `bank` varchar(30) NOT NULL,
+  `sts_usr` varchar(1) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id_usr`, `nm_usr`, `log_usr`, `pas_usr`, `email_usr`, `almt_usr`, `kp_usr`, `kota_usr`, `tlp`, `rek`, `nmrek`, `bank`, `sts_usr`) VALUES
+(28, 'Hakko Bio Richard', 'acchoblues', 'hakko270990', 'accho_blues@yahoo.co.id', 'Kp. Wangkal Rt.03 Rw.07 Desa kalijaya Kec. Cikarang barat', '17520', 'Kab. Bekasi', '085694984803', '121212121212', 'Hakko Bio Richard', 'BCA', ''),
+(27, 'Hakko Bio Richard', 'acchoblues', 'hakko270990', 'accho_blues@yahoo.co.id', 'Kp. Wangkal Rt.03 Rw.07 Desa kalijaya Kec. Cikarang barat', '17520', 'Kab. Bekasi', '085694984803', '121212121212', 'Hakko Bio Richard', 'Mandiri', ''),
+(29, 'Hakko Bio Richard', 'hakko bio richard', 'romance18', 'accho_blues@yahoo.co.id', 'Kp. Wangkal Rt.03 Rw.07 Desa kalijaya Kec. Cikarang barat', '17520', 'Kab. Bekasi', '085694984803', '121212121212', 'Hakko Bio Richard', 'Bank Jabar', '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`br_id`);
+
+--
+-- Indexes for table `pembelian`
+--
+ALTER TABLE `pembelian`
+  ADD PRIMARY KEY (`no_hdpem`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_usr`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `barang`
+--
+ALTER TABLE `barang`
+  MODIFY `br_id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_usr` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
